28947a2839ea2ebaa2d411a142cce8aa57d83b44
Evandru <evandro_remolli.t@outlook.com>
2023-07-27 13:09:23 -0300

    instantiate Pessoa and exhibition

--- aulajpa/src/Main.java
+++ aulajpa/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pessoa p1 = new Pessoa(1, "Carlos", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Joaquim", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Ana", "ana@gmail.com");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
     }
 }
