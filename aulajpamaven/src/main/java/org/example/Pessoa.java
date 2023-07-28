3bb8ad3efb227409eaf2a376022a6046f0703f3a
Evandru <evandro_remolli.t@outlook.com>
2023-07-28 16:13:28 -0300

    modify: added javax.persistence for Id and Entity

--- aulajpamaven/src/main/java/org/example/Pessoa.java
+++ aulajpamaven/src/main/java/org/example/Pessoa.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
