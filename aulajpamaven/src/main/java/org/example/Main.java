007ce0e99698cd2cb0275be9cae15ea7bf054926
Evandru <evandro_remolli.t@outlook.com>
2023-07-28 16:24:48 -0300

    pull entity from pessoa

--- aulajpamaven/src/main/java/org/example/Main.java
+++ aulajpamaven/src/main/java/org/example/Main.java
@@ -6,18 +6,14 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(null, "Carlos", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Joaquim", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Ana", "ana@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
     }
 }
